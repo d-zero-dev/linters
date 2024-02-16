4c1bf678e0d14301a17ca0b209ef5eb24410e59c
yusukehirao <yusukehirao@me.com>
2024-02-16 14:45:29 +0900

    Localize and improve warning messages for `selector-class-pattern` and `selector-nested-pattern`

--- packages/@d-zero/stylelint-config/name.js
+++ packages/@d-zero/stylelint-config/name.js
@@ -9,8 +9,32 @@ module.exports = {
 			'^c-[a-z][a-z0-9]*(?:-[a-z0-9]+)*(?:__[a-z0-9]+(?:-[a-z0-9]+)*)?$',
 			{
 				resolveNestedSelectors: true,
+				/**
+				 * @param {string} selector
+				 * @returns {string}
+				 */
+				message: (selector) => {
+					if (!selector.startsWith('.c-')) {
+						return `クラス名は「c-」から始めてください: ${selector}`;
+					}
+					if (selector.split('__').length > 2) {
+						return `「__」はコンポーネント名とエレメント名の区切りを表します。エレメント名の文字区切りは「-」を使います: ${selector}`;
+					}
+					return `クラス名に命名規則にない文字が含まれています: ${selector}`;
+				},
 			},
 		],
-		'selector-nested-pattern': '^[^.]+.*',
+		'selector-nested-pattern': [
+			'^[^.]+.*',
+			{
+				/**
+				 * @param {string} selector
+				 * @returns {string}
+				 */
+				message: (selector) => {
+					return `コンポーネントのスタイル定義の中で別のコンポーネントを定義してはいけません: ${selector}`;
+				},
+			},
+		],
 	},
 };
