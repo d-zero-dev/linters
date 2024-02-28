fdeed87ee24146833375b1861c489984dda2a7d1
yusukehirao <yusukehirao@me.com>
2024-02-28 17:01:28 +0900

    Implement `getValueType` utility function using CSSTree

---
+++ packages/@d-zero/stylelint-rules/src/utils/get-value-type.spec.ts
@@ -0,0 +1,36 @@
+import type { Rule, Declaration } from 'postcss';
+
+import postcss from 'postcss';
+import { describe, test, expect } from 'vitest';
+
+import { getValueType } from './get-value-type.js';
+
+function p(css: string) {
+	const root = postcss.parse(css);
+	const rule = root.first as Rule;
+	const decl = rule.first as Declaration;
+	const nodeWithType = getValueType(decl);
+	return nodeWithType.map((node) => node.valueType);
+}
+
+describe('getValueType', () => {
+	test('flex', () => {
+		expect(p('a { flex: 1 1 calc(var(--foo) * 10vw) }')).toEqual([
+			'number',
+			'number',
+			'length',
+		]);
+	});
+
+	test('max-width', () => {
+		expect(p('a { max-width: 10% }')).toEqual(['length-percentage']);
+	});
+
+	test('height vw', () => {
+		expect(p('a { height: 100vw }')).toEqual(['length']);
+	});
+
+	test('SASS Variable', () => {
+		expect(p('a { flex: 1 2 $basis }')).toEqual(['number', 'number', '$SASS_VARIABLE']);
+	});
+});
---
+++ packages/@d-zero/stylelint-rules/src/utils/get-value-type.ts
@@ -0,0 +1,76 @@
+import type { KeywordToken, PropertyToken, TypeToken } from '@d-zero/csstree-scss-syntax';
+import type { Declaration } from 'postcss';
+
+import CSSTree from '@d-zero/csstree-scss-syntax';
+import postcssValueParser from 'postcss-value-parser';
+
+export function getValueType(decl: Declaration) {
+	return _getValueType(decl.prop, decl.value);
+}
+
+function _getValueType(
+	prop: string,
+	value: string,
+): {
+	value: postcssValueParser.Node;
+	valueType: string | null;
+}[] {
+	const valueAst = postcssValueParser(value);
+	const valueAstFromCssTree = CSSTree.parse(value, { context: 'value' });
+	let cssTreeDecl = CSSTree.lexer.matchProperty(prop, valueAstFromCssTree);
+
+	if (cssTreeDecl.error?.message === 'Matching for a tree with var() is not supported') {
+		value = value.replaceAll(
+			/(var\([^)]+\))/g,
+			(_, $1) => ' '.repeat($1.length - 1) + '1',
+		);
+		cssTreeDecl = CSSTree.lexer.matchProperty(prop, value);
+	}
+
+	const values = valueAst.nodes.filter(
+		(node) => node.type === 'string' || node.type === 'function' || node.type === 'word',
+	);
+
+	const props = cssTreeDecl.matched;
+	if (props === null) {
+		throw cssTreeDecl.error;
+	}
+
+	const valueTypes = props.match.map((node) => getValueNode(node).syntax.name);
+
+	return values.map((value, i) => {
+		const valueType = valueTypes[i] ?? null;
+
+		if (valueType === null) {
+			if (value.type === 'word' && value.value.startsWith('$')) {
+				return {
+					value: value,
+					valueType: '$SASS_VARIABLE',
+				};
+			}
+
+			throw new Error('Value type not found');
+		}
+
+		return {
+			value,
+			valueType,
+		};
+	});
+}
+
+function getValueNode(
+	node: PropertyToken | TypeToken | KeywordToken,
+): TypeToken | KeywordToken {
+	if (isProperty(node)) {
+		const firstChild = node.match.at(0)!;
+		return getValueNode(firstChild);
+	}
+	return node;
+}
+
+function isProperty(
+	node: PropertyToken | TypeToken | KeywordToken,
+): node is PropertyToken {
+	return node.syntax.type === 'Property';
+}
