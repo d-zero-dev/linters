7c87d32f1f5a6b89565f79cb08f51ed03998ba0a
YusukeHirao <hirao@d-zero.co.jp>
2024-10-09 14:54:43 +0900

    fix(markuplint): disable `required-attr` to `script[src^='https://']`

--- packages/@d-zero/markuplint-config/base.js
+++ packages/@d-zero/markuplint-config/base.js
@@ -2,7 +2,7 @@ export default {
 	extends: ['markuplint:recommended-static-html'],
 	nodeRules: [
 		{
-			selector: "script[src^='https://']",
+			selector: "script[src^='https://'], script[src^='https://']",
 			rules: {
 				'required-attr': false,
 			},
