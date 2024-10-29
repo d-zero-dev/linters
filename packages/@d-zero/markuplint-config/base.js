3771bbce0c5e40a5a9e434f4ef35b409218e6da6
YusukeHirao <hirao@d-zero.co.jp>
2024-10-29 14:48:57 +0900

    feat(markuplint): br要素原則禁止を追加

--- packages/@d-zero/markuplint-config/base.js
+++ packages/@d-zero/markuplint-config/base.js
@@ -3,6 +3,13 @@
  */
 export default {
 	extends: ['markuplint:recommended-static-html'],
+	rules: {
+		'disallowed-element': {
+			value: ['br'],
+			reason:
+				'br要素は原則使用しません。代わりにCSSでスタイルを調整してください。使用する場合は場合は理由が必要です。（D-ZERO独自ルール）',
+		},
+	},
 	nodeRules: [
 		{
 			selector: "script[src^='https://'], script[src^='https://']",
