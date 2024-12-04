e10df266ffdb87e1d0424a7db8be72e29bf7ebac
YusukeHirao <hirao@d-zero.co.jp>
2024-12-04 21:46:22 +0900

    feat(markuplint): add `extendsConfig` function

--- packages/@d-zero/markuplint-config/index.js
+++ packages/@d-zero/markuplint-config/index.js
@@ -1,38 +1,53 @@
 import base from './base.js';
-import name from './name.js';
+import nameConfig, { nameWith } from './name.js';
 import pug from './pug.js';
+
+const createConfig = (name = nameConfig) => {
+	return {
+		...base,
+		...pug,
+		...name,
+		parser: {
+			...base.parser,
+			...pug.parser,
+			...name.parser,
+		},
+		rules: {
+			...base.rules,
+			...pug.rules,
+			...name.rules,
+		},
+		nodeRules: [
+			//
+			...(base.nodeRules ?? []),
+			...(pug.nodeRules ?? []),
+			...(name.nodeRules ?? []),
+		],
+		childNodeRules: [
+			//
+			...(base.childNodeRules ?? []),
+			...(pug.childNodeRules ?? []),
+			...(name.childNodeRules ?? []),
+		],
+		overrideMode: 'merge',
+		overrides: {
+			...pug.overrides,
+		},
+	};
+};
 
 /**
  * @type {import('@markuplint/ml-config').Config}
  */
-export default {
-	...base,
-	...pug,
-	...name,
-	parser: {
-		...base.parser,
-		...pug.parser,
-		...name.parser,
-	},
-	rules: {
-		...base.rules,
-		...pug.rules,
-		...name.rules,
-	},
-	nodeRules: [
-		//
-		...(base.nodeRules ?? []),
-		...(pug.nodeRules ?? []),
-		...(name.nodeRules ?? []),
-	],
-	childNodeRules: [
-		//
-		...(base.childNodeRules ?? []),
-		...(pug.childNodeRules ?? []),
-		...(name.childNodeRules ?? []),
-	],
-	overrideMode: 'merge',
-	overrides: {
-		...pug.overrides,
-	},
+export default createConfig();
+
+/**
+ *
+ * @param {Object} options
+ * @param {string[]?} options.classNaming
+ * @returns
+ */
+export const extendsConfig = (options) => {
+	const name = options?.classNaming ? nameWith(options.classNaming) : nameConfig;
+	return createConfig(name);
 };
