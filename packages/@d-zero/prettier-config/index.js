55838c3ca7cfd9254dd2dc5d5e6e10f41a99dbb6
YusukeHirao <hirao@d-zero.co.jp>
2025-06-26 13:34:16 +0900

    refactor(prettier): split configuration into base and pug modules
    - Extract base prettier configuration to base.js
    - Extract pug-specific configuration to pug.js
    - Update main config to compose from base and pug modules
    - Add new exports for base and pug configurations in package.json
    - Improve modularity and reusability of prettier configurations

--- packages/@d-zero/prettier-config/index.js
+++ packages/@d-zero/prettier-config/index.js
@@ -1,43 +1,9 @@
+import base from './base.js';
+import pug from './pug.js';
+
 export default {
-	plugins: ['@prettier/plugin-pug'],
-
-	arrowParens: 'always',
-	bracketSameLine: true,
-	bracketSpacing: true,
-	jsxSingleQuote: false,
-	printWidth: 90,
-	quoteProps: 'as-needed',
-	semi: true,
-	singleQuote: true,
-	tabWidth: 2,
-	trailingComma: 'all',
-	useTabs: true,
-
-	pugAttributeSeparator: 'as-needed',
-	pugCommentPreserveSpaces: 'trim-all',
-	pugEmptyAttributes: 'none',
-	pugSingleQuote: false,
-	pugSortAttributesBeginning: ['class'],
-	pugSortAttributesEnd: ['id'],
-
-	overrides: [
-		{
-			files: '.*rc',
-			options: { parser: 'json' },
-		},
-		{
-			files: '*.html',
-			options: {
-				printWidth: 100_000,
-			},
-		},
-		{
-			files: '*.mdc',
-			options: { parser: 'markdown' },
-		},
-		{
-			files: '.clinerules',
-			options: { parser: 'markdown' },
-		},
-	],
+	...base,
+	...pug,
+	plugins: [...(base.plugins ?? []), ...(pug.plugins ?? [])],
+	overrides: [...(base.overrides ?? []), ...(pug.overrides ?? [])],
 };
---
+++ packages/@d-zero/prettier-config/pug.js
@@ -0,0 +1,9 @@
+export default {
+	plugins: ['@prettier/plugin-pug'],
+	pugAttributeSeparator: 'as-needed',
+	pugCommentPreserveSpaces: 'trim-all',
+	pugEmptyAttributes: 'none',
+	pugSingleQuote: false,
+	pugSortAttributesBeginning: ['class'],
+	pugSortAttributesEnd: ['id'],
+};
