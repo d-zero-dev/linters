0c2a4378fb153d51d44c1789ab9644a2667b0c03
Yusuke Hirao <hirao@d-zero.co.jp>
2026-08-19 04:11:22 +0900

    refactor(eslint): remove typescript-eslint and delegate typed rules to oxlint

--- packages/@d-zero/eslint-config/commonjs.js
+++ packages/@d-zero/eslint-config/commonjs.js
@@ -4,10 +4,6 @@ import globals from 'globals';
  * @type {import('eslint').Linter.Config}
  */
 export const commonjs = {
-	rules: {
-		'unicorn/prefer-module': 0,
-		'@typescript-eslint/no-require-imports': 0,
-	},
 	languageOptions: {
 		globals: {
 			...globals.commonjs,
--- packages/@d-zero/eslint-config/index.js
+++ packages/@d-zero/eslint-config/index.js
@@ -1,19 +1,14 @@
 import { base } from './base.js';
 import { commonjs } from './commonjs.js';
 import { frontend } from './frontend.js';
-import { ts } from './typescript.js';
 
 /**
  * @type {import('eslint').ESLint.Plugin}
  */
 export default {
 	configs: {
-		standard: [...ts],
 		base: [...base],
-		node: [...ts],
-		nodeNoTS: [...base],
-		frontend: [...ts, frontend],
-		frontendNoTS: [...base, frontend],
+		frontend: [...base, frontend],
 		commonjs,
 	},
 };
